0c930e0b1f751b63263a825f203eeae7684b067c
Sean Herron <sean@herron.io>
2014-08-12 22:22:18 -0400

    Remove copyright template

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,7 @@
 #
 # Cookbook Name:: packer
 # Recipe:: default
-#
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+
 
 include_recipe 'apt'
 include_recipe 'nginx'
