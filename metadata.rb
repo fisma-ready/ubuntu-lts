052a50534107f4a3d4372032a46ca79809cb9f02
Diego Lapiduz <diego.lapiduz@gsa.gov>
2014-12-18 15:22:17 -0800

    Remove nginx from base AMI

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures packer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w{ apt nginx }.each do |cookbook|
+%w{ apt }.each do |cookbook|
   depends cookbook
 end
 
